edcaafa9c7b55cce6b8cab7ebe133f1d23dec7b6
Moringa School <Ianwanarua>
2021-09-12 20:02:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".design").click(function() {
+      $("#designtext").toggle();
+      $("#design_img").toggle();
+    });
+  });
+
+  $(document).ready(function() {
+    $(".development").click(function() {
+      $("#developmenttext").toggle();
+      $("#development_img").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".product").click(function() {
+      $("#producttext").toggle();
+      $("#product_img").toggle();
+    });
+  });
